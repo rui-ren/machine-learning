dd64e8cfa6fd6d9479414dd7b875bc8a0b0dc8c6
ruirenjerrry@gmail.com <ruirenjerry@gmail.com>
2017-12-27 09:32:32 -0600

    SAX

---
+++ SAX.py
@@ -0,0 +1,60 @@
+def znormalization(ts):
+    # ts- each column of ts is a time series 
+    nus = ts.mean(axis = 0)
+    stds = ts. std(axis = 0)
+    return (ts - mus) / stds
+
+import numpy as np
+ts1 = np.asarray([2.02, 2.33, 2.99, 6.85, 9.20, 8.80, 7.50, 6.00, 5.85, 3.85, 4.85, 3.85, 2.22, 1.45, 1.34])
+ts2 = np.asarray([0.50, 1.29, 2.58, 3.83, 3.25, 4.25, 3.83, 5.63, 6.44, 6.25, 8.75, 8.83, 3.25, 0.75, 0.72])
+ts = pd.DataFrame({"ts1": ts1, "ts2": ts2})
+ts.plot(style = "-+")
+zts = znormalization(ts)
+zts.plot(style = "-+")
+plt.hlines(0, 0, 14, colors = 'r', linestyles='--')
+
+def paa_transform(ts, n_pieces):
+    """
+    ts: the columns of which are time series represented by e.g. np.array
+    n_pieces: M equally sized piecies into which the original ts is splitted
+    """
+    splitted = np.array_split(ts, n_pieces) ## along columns as we want
+    return np.asarray(map(lambda xs: xs.mean(axis = 0), splitted))
+
+split9 = paa_transform(zts, 5)
+split9_ext = np.repeat(split9, 3, axis = 0)
+for i in [0, 1]:
+    pl.figure()
+    pl.plot(zts.iloc[:, i], '-+', label = "ts%i"%i)
+    pl.plot(split9_ext[:, i], label = "paa%i"%i)
+    pl.legend(loc = "upper left")
+
+
+    def sax_transform(ts, n_pieces, alphabet):
+    """
+    ts: columns of which are time serieses represented by np.array
+    n_pieces: number of segments in paa transformation
+    alphabet: the letters to be translated to, e.g. "abcd", "ab"
+    return np.array of ts's sax transformation
+    Steps:
+    1. znormalize
+    2. ppa
+    3. find norm distribution breakpoints by scipy.stats
+    4. convert ppa transformation into strings
+    """
+    from scipy.stats import norm
+    alphabet_sz = len(alphabet)
+    thrholds = norm.ppf(np.linspace(1./alphabet_sz, 
+                                    1-1./alphabet_sz, 
+                                    alphabet_sz-1))
+    def translate(ts_values):
+        return np.asarray([(alphabet[0]
+    if ts_value < thrholds[0]
+        else (alphabet[-1]
+    if ts_value > thrholds[-1]
+        else alphabet[np.where(thrholds <= ts_value)[0][-1]+1]))
+    for ts_value in ts_values])
+        paa_ts = paa_transform(znormalization(ts), n_pieces)
+    return np.apply_along_axis(translate, 0, paa_ts)
+
+sax_transform(ts, 9, "abcd")
